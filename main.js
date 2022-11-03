4987d6c84dd157a12338763a3e8332d5df898a87
lucca <lucca.f.bittencourt@gmail.com>
2022-11-03 13:28:07 -0300

    refactor

--- main.js
+++ main.js
@@ -19,18 +19,12 @@ function createCard(date, day, games) {
   `
 }
 
-document.querySelector("#app").innerHTML = `
-   <header>
-      <img src="./assets/logo.svg" alt="Logo da NLW" />
-    </header>
-    <main id="cards">
-     ${createCard("24/11", "quinta", createGame("brazil", "16:00", "serbia"))}
-     ${createCard(
-       "28/11",
-       "segunda",
-       createGame("brazil", "13:00", "switzerland") +
-         createGame("portugal", "16:00", "uruguay")
-     )} 
-     ${createCard("02/12", "sexta", createGame("brazil", "16:00", "cameroon"))}
-    </main>
-`
+document.querySelector("#cards").innerHTML =
+  createCard("24/11", "quinta", createGame("brazil", "16:00", "serbia")) +
+  createCard(
+    "28/11",
+    "segunda",
+    createGame("brazil", "13:00", "switzerland") +
+      createGame("portugal", "16:00", "uruguay")
+  ) +
+  createCard("02/12", "sexta", createGame("brazil", "16:00", "cameroon"))
